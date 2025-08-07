c9644984a6ae550e1d94e2b5c26a01740e5d573c
Jennifer <jtlewis204@gmail.com>
2025-08-07 13:23:12 -0500

    Update vo2.py

--- src/utils/vo2.py
+++ src/utils/vo2.py
@@ -14,3 +14,23 @@ def calculate_vo2_max(avg_speed: float, hr_max: float, hr_rest: float, hr_avg: f
     heart_rate_factor = (hr_max - hr_rest) / (hr_max - hr_avg)
     vo2_max = 15.3 * heart_rate_factor * speed_kmh
     return round(min(max(vo2_max, 2), 95), 2)
+    
+"""
+replace with this equation
+
+def vo2max_daniels_hr(pace_min_per_km, hr_avg, hr_max, hr_rest):
+    if pace_min_per_km <= 0 or hr_max == hr_rest:
+        return 0.0
+
+    speed_m_per_min = 1000 / pace_min_per_km
+    vo2_at_pace = 3.5 + 0.2 * speed_m_per_min
+
+    hr_reserve = hr_max - hr_rest
+    relative_effort = (hr_avg - hr_rest) / hr_reserve
+
+    if relative_effort <= 0:
+        return 0.0
+
+    vo2max = vo2_at_pace / relative_effort
+    return round(min(max(vo2max, 20), 95), 2)
+"""
