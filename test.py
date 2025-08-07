0f322a8c05986029ae48590857477a50005fef5a
Jennifer <jtlewis204@gmail.com>
2025-08-07 12:44:45 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import requests
 
-CLIENT_ID = '167815'
-CLIENT_SECRET = '8b868cd57010e08192a8bbd44edb841a1f0c9b59'
-CODE = '19711e68213b5044150423fbf844c85ba34aecca'
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+CODE = ''
 
 res = requests.post('https://www.strava.com/oauth/token', data={
     'client_id': CLIENT_ID,
